a4f56b268288954ac3a0fd16ec23de626accdb77
성현 <supersh0106@gmail.com>
2025-09-29 12:09:27 +0900

    main.go수정

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,16 +6,12 @@ import (
 )
 
 func main() {
-	// var name string
-	// name = "Kim seong hyun"
-
-	// var name = "Kim seong hyun"
-
-	// name := "Kim seong hyun"
-
-	// name := 2.71
-
-	var name float64
-	name = 2.71
-	fmt.Println(name, reflect.TypeOf(name))
+	var f64 float64
+	var str string
+	var i32 int32
+	var b bool
+	fmt.Println(f64, reflect.TypeOf(f64))
+	fmt.Println(str, reflect.TypeOf(str))
+	fmt.Println(i32, reflect.TypeOf(i32))
+	fmt.Println(b, reflect.TypeOf(b))
 }
