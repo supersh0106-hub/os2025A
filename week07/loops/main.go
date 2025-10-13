abcd8b5044ef5b94cedca77c5e8d673e7d7606c0
성현 <supersh0106@gmail.com>
2025-10-13 11:24:48 +0900

    loops/main.go v0.1 bufio & os package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
-	"time"
+	"os"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	var day int = now.Day()
-	fmt.Println(day)
-	univ := "Go$ Inha$"
-	changer := strings.NewReplacer("$", "!")
-	changed := changer.Replace(univ)
-	fmt.Println(changed)
+	r := bufio.NewReader(os.Stdin)
+	i, _ := r.ReadString('\n') // ignore error
+	fmt.Println(i)
 }
